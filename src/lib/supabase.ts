44207f54b887e963f639fb06ae8bacd36e03a941
Israfil008 <162714081+Israfil008@users.noreply.github.com>
2026-03-15 12:38:50 +0545

    Delete supabase.ts

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://qakiedxiolwtsvochkyi.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFha2llZHhpb2x3dHN2b2Noa3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0NzkyMzksImV4cCI6MjA4OTA1NTIzOX0.2JPJuMRPCnPsfA5-PvbAc7ufWdzKQRBvoBOZYmE2gq0";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
